25108dfa4d65f877dc030f3e86e44d560117f4e3
Shazzadul-Shakib <shakib1186@gmail.com>
2024-05-17 13:02:10 +0600

    check if git ignore wored properly

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ export const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/",async(req,res)=>{
+
+app.get("/",(_,res)=>{
     res.status(200).json({message:"Server is running"});
 })
