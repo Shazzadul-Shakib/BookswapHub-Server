44eb3facb9df07b4a00feabd51294f3791b68e7f
shakib-shazzad <shakib1186@gmail.com>
2024-06-10 00:18:09 +0600

    vercel json file added

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,6 @@ import path from "path";
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 export const config = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 5000,
   MONGO_URI: process.env.MONGO_URI,
 };
